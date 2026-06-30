172c03c04c923b565a599325e01f392caefbc163
Iris Johansene Cempron <irisjohansene@users.noreply.github.com>
2026-06-30 17:07:17 +0800

    Force clean Render client builds

--- render-build-client.sh
+++ render-build-client.sh
@@ -7,6 +7,9 @@ if [ ! -x "${DOTNET_DIR}/dotnet" ]; then
   bash /tmp/dotnet-install.sh --channel 8.0 --install-dir "${DOTNET_DIR}"
 fi
 
+"${DOTNET_DIR}/dotnet" clean HRDocs.Client/HRDocs.Client.csproj \
+  --configuration Release
+
 "${DOTNET_DIR}/dotnet" publish HRDocs.Client/HRDocs.Client.csproj \
   --configuration Release \
   --output publish-client
